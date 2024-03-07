102d36a420ff83bde0ae6b262c5d30c78acdf4cf
Evelynewaters <eveclaraify@gmail.com>
2024-03-06 22:23:10 -0800

    Docs- this commit contains solutions to task 0-1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,38 @@
 #!/usr/bin/env bash
-#Sets up web servers for deployment of web_static
+# Sets up a web server for deployment of web_static.
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
+apt-get update
+apt-get install -y nginx
+
 mkdir -p /data/web_static/releases/test/
-printf "<html>
-  <head>
-  </head>
-  <body>
-    Best School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server;/a location  /hbnb_static { alias /data/web_static/current; autoindex off; }' /etc/nginx/sites-available/default
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+	alias /data/web_static/current;
+	index index.html index.htm;
+    }
+
+    location /redirect_me {
+	return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
 service nginx restart
